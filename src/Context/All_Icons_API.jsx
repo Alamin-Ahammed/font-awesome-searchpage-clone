91258635d19760b29f5234118af6dd40e7e29c29
Alamin-Ahammed <ahammedalamin2002@gmail.com>
2024-02-15 20:17:34 +0600

    made search result more accurate

--- src/Context/All_Icons_API.jsx
+++ src/Context/All_Icons_API.jsx
@@ -1,24 +1,33 @@
-import { createContext, useContext, useMemo, useState } from "react";
+import { createContext, useContext, useMemo } from "react";
 import { useFilterData } from "./FilterDataAndIconDataStorage";
 import { SVGData } from "../SVGData";
 const IconsStorage = createContext();
 
 export const All_Icons_API_Provider = ({ children }) => {
-  // const [AllIcons, setAllIcons] = useState({});
-  const svgData = SVGData; // this will come from the database as an array of objects of icons and its details.
+  const svgData = SVGData;
   const { FilterDataAndIconData } = useFilterData();
+
   const filteredResult = useMemo(() => {
     if (Object.keys(FilterDataAndIconData).length === 0) {
       return svgData;
     }
-    return svgData.filter((item) =>
-      Object.keys(FilterDataAndIconData).every(
-        (key) => item[key] === FilterDataAndIconData[key]
-      )
-    );
+    return svgData.filter((item) => {
+      return Object.keys(FilterDataAndIconData).every((key) => {
+        const filterValue = FilterDataAndIconData[key].toLowerCase();
+        const iconValue = item[key].toLowerCase();
+
+        // Checking if the filter property is 'name' tehn performing partial match
+        if (key === "iconName") {
+          console.log(key);
+          return iconValue.includes(filterValue);
+        }
+
+        // For other properties, performing exact match
+        return iconValue === filterValue;
+      });
+    });
   }, [FilterDataAndIconData, svgData]);
 
-  // setAllIcons(filteredResult);
   const AllIcons = filteredResult;
 
   return (
